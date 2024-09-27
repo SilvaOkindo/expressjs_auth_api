181d6b882b892715e61531600772e9c911085db6
Silvanus Okindo <okindosilva@gmail.com>
2024-09-27 08:44:35 +0300

    first commit

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,18 +1,27 @@
 const express = require("express")
+const verifyToken = require("../middleware/authMiddleware")
+const authorizeRoles = require("../middleware/rolesMiddleware")
 
 const router = express.Router()
 
 
-router.get("/admin", (req, res) => {
+router.get("/admin", 
+    verifyToken, 
+    authorizeRoles("admin"), 
+     (req, res) => {
     res.json({message: "admin router"})
 })
 
-router.get("/user", (req, res) => {
+router.get("/user", verifyToken,
+     authorizeRoles("user", "manager", "admin"),
+     (req, res) => {
     res.json({message: "user router"})
 })
 
-router.get("/manager", (req, res) => {
-    res.json({message: "manager router"})
+router.get("/manager",
+     verifyToken, authorizeRoles("admin", "manager"),
+      (req, res) => {
+    res.json({message: "manager router"}) 
 })
 
 
